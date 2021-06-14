e99993c1c22041c1ba8715fce9e7f714aa119bd1
Nayan's <nayanguptaa10@gmail.com>
2021-06-14 16:04:35 +0530

    preparing the brushsizeselector and how to use displayMatrics

--- app/src/main/java/com/nayan/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/nayan/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.nayan.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -29,7 +30,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+    //    mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -46,7 +47,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         for (path in mPaths) {
             mDrawPaint!!.strokeWidth = path.brushThickness
             mDrawPaint!!.color = path.color
-            canvas?.drawPath(path,mDrawPaint!!)
+            canvas?.drawPath(path, mDrawPaint!!)
         }
         if (!mDrawPath!!.isEmpty) {
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
@@ -86,6 +87,17 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         }
         invalidate()
         return true
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize =
+            TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                newSize,
+                resources.displayMetrics
+            )
+        mDrawPaint!!.strokeWidth=mBrushSize
+
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
